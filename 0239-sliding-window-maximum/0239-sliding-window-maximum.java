c3379a6224a7119a9ad96f89be0fa4dfc53d25c7
Bhavya Singla <145319363+Bhavya124@users.noreply.github.com>
2025-12-24 22:02:07 +0530

    79 ms (13.19%) 159.1 MB (5.34%) 12-24-2025 0239-sliding-window-maximum Hard .java

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,16 +1,28 @@
+class pair{
+    int val;
+    int index;
+    pair(int v , int i){
+        val =  v;
+        index = i;
+    }
+}
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<pair> pq = new PriorityQueue<>(
+            (a , b) -> b.val - a.val
+        );
         int n = nums.length;
         int[] arr = new int[n -k+1];
         for(int i = 0 ; i < k ; i++){
-            pq.add(nums[i]);
+            pq.add(new pair(nums[i] , i));
         }
-        arr[0] = pq.peek();
+        arr[0] = pq.peek().val;
         for(int i = k ; i < n ; i++){
-            pq.remove(nums[i - k]);
-            pq.add(nums[i]);
-            arr[i -k + 1] = pq.peek();
+            pq.add(new pair(nums[i] , i));
+            while(pq.peek().index <= i - k){
+                pq.poll();
+            }
+            arr[i -k + 1] = pq.peek().val;
         }
         return arr;             
     }

ae36da06e841fb460255f125cd23a670aa276ddf
Bhavya Singla <145319363+Bhavya124@users.noreply.github.com>
2025-12-25 23:38:09 +0530

    9 ms (15.4%) 48.3 MB (18.26%) 12-25-2025 0240-search-a-2d-matrix-ii Medium .java

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         for(int i = 0 ; i < matrix.length ; i++){
-            if(target > matrix[i][0]){
+            if(target >= matrix[i][0]){
             for(int j = 0 ; j < matrix[0].length ; j++){
                 if(matrix[i][j] == target){
                     return true;

b8c3acd4cd38a459a8a00d608f29463a5f5ff836
Bhavya Singla <145319363+Bhavya124@users.noreply.github.com>
2026-01-03 22:10:18 +0530

    1 ms (98.79%) 46.4 MB (57.29%) 01-03-2026 3736-minimum-moves-to-equal-array-elements-iii Easy .java

--- 3736-minimum-moves-to-equal-array-elements-iii/3736-minimum-moves-to-equal-array-elements-iii.java
+++ 3736-minimum-moves-to-equal-array-elements-iii/3736-minimum-moves-to-equal-array-elements-iii.java
@@ -1,9 +1,13 @@
 class Solution {
     public int minMoves(int[] nums) {
-        Arrays.sort(nums);
         int n = nums.length - 1;
-        int maxi = nums[n];
+        int maxi = 0;
         int count = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] > maxi){
+                maxi = nums[i];
+            }
+        }
         for(int i = 0 ; i <= n ; i++){
             if(nums[i] < maxi){
                 count+= maxi - nums[i];

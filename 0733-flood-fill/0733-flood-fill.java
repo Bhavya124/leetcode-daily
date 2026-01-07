000ae64ef7b6af41cfe05ecbe2de9ce26bac8838
Bhavya Singla <145319363+Bhavya124@users.noreply.github.com>
2026-01-07 18:26:39 +0530

    0 ms (100%) 46.9 MB (45.13%) 01-07-2026 0733-flood-fill Easy .java

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,41 +1,25 @@
-class pair{
-    int row;
-    int col;
-    pair(int r , int c){
-        row = r;
-        col = c;
-    }
-}
 class Solution {
-    public int[][] directions = new int[][]{
-        {0 , 1},
-        {0 , -1},
-        {1 , 0},
-        {-1 , 0},
-    };
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        Queue<pair> q = new ArrayDeque<>();
-        int check = image[sr][sc];
-        if(check == color) return image;
-        image[sr][sc] = color;
-        q.add(new pair(sr , sc));
-        while(!q.isEmpty()){
-            pair curr = q.poll();
-            for(int[] dir : directions){
-                int newRow = curr.row + dir[0];
-                int newCol = curr.col + dir[1];
-
-                if(newRow < 0 || newRow >= image.length || newCol < 0 || newCol >= image[0].length) continue;
-                if(image[newRow][newCol] == check){
-                    image[newRow][newCol] = color;
-                    q.add(new pair(newRow , newCol));
-                }
-            }
+        int oldColor = image[sr][sc];
+        if(oldColor == color){
+            return image;
         }
-
+        flood(image , sr , sc , color ,oldColor ,  image.length , image[0].length);
         return image;
+    }
 
-        
+    public void flood(int[][] image , int sr , int sc , int color, int oldColor, int n , int m){
+        if(sr < 0 || sc < 0 || sr >= n || sc >= m){
+            return;
+        }
+        if(image[sr][sc] != oldColor){
+            return;
+        }
+        image[sr][sc] = color;
+        flood(image , sr + 1 , sc , color , oldColor , n , m);
+        flood(image , sr - 1 , sc , color , oldColor , n , m);
+        flood(image , sr , sc + 1 , color , oldColor , n , m);
+        flood(image , sr , sc - 1 , color , oldColor , n , m);
 
     }
 }

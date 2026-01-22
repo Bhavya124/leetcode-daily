8284306b0aadf9f44f88c548df37bdc5b0c3813c
Bhavya Singla <145319363+Bhavya124@users.noreply.github.com>
2026-01-22 12:49:22 +0530

    74 ms (28.27%) 168.5 MB (24.51%) 01-22-2026 0300-longest-increasing-subsequence Medium .java

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,20 +1,33 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        if(nums.length == 1){
-            return 1;
-        }
-        int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp , 1);
-        int maxi = 0;
-        for(int i = 1 ; i < n ; i++){
-            for(int j = 0 ; j < i ; j++){
-                if(nums[j] < nums[i]){
-                    dp[i] = Math.max(dp[i] , dp[j] + 1);
-                }
+    int[] nums1 = new int[nums.length];
+    for(int i = 0 ; i < nums.length ; i++){
+        nums1[i] = nums[i];
+    }
+    
+    HashSet<Integer> set = new HashSet<>();
+    for(int i = 0 ; i < nums.length ; i++){
+        set.add(nums[i]);
+    }
+    int[] nums2 = new int[set.size()];
+    int k = 0;
+    for(int it : set){
+        nums2[k++] = it;
+    }
+    Arrays.sort(nums2);
+    int n = nums1.length;
+    int m = nums2.length;
+    int[][] dp = new int[n + 1][m + 1];
+    for(int i = 1 ; i < n + 1 ; i++){
+        for(int j = 1 ; j < m + 1 ; j++){
+            if(nums1[i - 1] == nums2[j - 1]){
+                dp[i][j] = 1 + dp[i - 1][j - 1];
             }
-            maxi = Math.max(dp[i] , maxi);
+            else{
+                dp[i][j] = Math.max(dp[i - 1][j] , dp[i][j - 1]);
+            }
         }
-        return maxi;
+    }
+    return dp[n][m];   
     }
 }

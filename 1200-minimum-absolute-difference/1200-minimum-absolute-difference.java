b1cdb1414b1e501dec9c692a88c7fde3d6f52b00
Bhavya Singla <145319363+Bhavya124@users.noreply.github.com>
2026-01-26 23:35:57 +0530

    20 ms (97.68%) 64.2 MB (20.2%) 01-26-2026 1200-minimum-absolute-difference Easy .java

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -4,7 +4,14 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         int n = arr.length;
         int i = 0;
-        int mini = Math.abs(arr[1] - arr[0]);
+        int j = 0;
+        int mini = Integer.MAX_VALUE;
+        while(j < n - 1){
+            if(arr[j + 1] - arr[j] <= mini){
+                mini = Math.abs(arr[j + 1] - arr[j]);
+            }
+            j++; 
+        }
         while(i < n - 1){
             if(arr[i + 1] - arr[i] == mini){
                 List<Integer>temp = new ArrayList<>();

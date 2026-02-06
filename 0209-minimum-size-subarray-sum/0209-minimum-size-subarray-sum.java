65cd2869243b5dec96983ccd777c2547dd6c18b6
Bhavya Singla <145319363+Bhavya124@users.noreply.github.com>
2026-02-06 17:44:29 +0530

    1 ms (99.64%) 62.2 MB (47.09%) 02-06-2026 0209-minimum-size-subarray-sum Medium .java

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int i = 0;
+        int j = 0;
+        int mini = Integer.MAX_VALUE;
+        int sum = 0;
+        int n = nums.length;
+        while(j < nums.length){
+            sum = sum + nums[j];
+            while(sum >= target){
+                mini = Math.min(mini , j - i + 1);
+                sum = sum - nums[i];
+                i++;    
+            }
+            j++;
+        }
+        if(mini == Integer.MAX_VALUE){
+            return 0;
+        }
+        return mini;
+    }
+}

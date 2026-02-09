c553542fe09c0efe4a2a755062c9df29f895a490
Bhavya Singla <145319363+Bhavya124@users.noreply.github.com>
2026-02-09 17:44:59 +0530

    0 ms (100%) 52.8 MB (75.64%) 02-09-2026 0540-single-element-in-a-sorted-array Medium .java

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int n = nums.length - 1;
+
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(mid == 0 || mid == n) return nums[mid];
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
+            if(mid %2 == 0){
+                if(mid < n && nums[mid] == nums[mid + 1]){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+            else{
+                if(mid > 0 && nums[mid] == nums[mid-  1]){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+
+
+        }
+        return nums[left];
+    }
+}
